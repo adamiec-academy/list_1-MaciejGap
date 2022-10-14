becd61918f2c1b2fc252ce5f616c94a5e429aad1
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-10-14 19:26:07 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,16 @@
-def chess_board(n, k):
-    pass
+
+
+def chess_board(n , k):
+    w = k * " "
+    b = k * "#"
+
+    for _ in range(n):
+        for _ in range(k):
+            for _ in range(n):
+                print(w + b, end="")
+            print()
+
+        for _ in range(k):
+            for _ in range(n):
+                print(b + w, end="")
+            print()
