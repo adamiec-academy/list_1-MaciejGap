c1575c18e956fc2fd434fe1e1fb91c9ec3c06bb8
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-10-15 14:15:22 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,17 @@
 def envelope(n):
-    pass
+    # segment 1
+    print((n * 2 + 1) * "*")
+
+    # segment 2
+    for i in range(n - 1):
+        print("*" + i * " " + "*" + (2 * n - 3 - 2 * i) * " " + "*" + i * " " + "*")
+
+    # Segment 3
+    print("*" + (n - 1) * " " + "*" + (n - 1) * " " + "*")
+
+    # Segment 4
+    for i in range(n - 1):
+        print("*" + (n - 2 - i) * " " + "*" + (2 * i + 1) * " " + "*" + (n - 2 -i) * " " + "*")
+
+    # Segment 5
+    print((n * 2 + 1) * "*")
